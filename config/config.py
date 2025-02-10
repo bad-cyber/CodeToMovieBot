abdd434fcb3d05a67abfdd665531535d1b700413
bad-cyber <emelyanov.nikita.2003@mail.ru>
2025-02-10 22:29:16 +0300

    добавил недостающие файлы

---
+++ config/config.py
@@ -0,0 +1,15 @@
+# Bot Configuration
+BOT_TOKEN = "7554992084:AAH-WMh-Ihv3Uqk39EW8PoFR3QjPkIEvS2c"
+CHANNEL_ID = "-1002296873924"
+
+# Github AutoUpdate Configuration
+GITHUB_TOKEN = "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"
+
+# Logging Configuration
+LOG_LEVEL = "INFO"
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+# Film Code Settings
+FILM_CODE_MIN = 100
+FILM_CODE_MAX = 100000
+MAX_ATTEMPTS = 10
