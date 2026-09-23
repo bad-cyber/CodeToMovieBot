e179d0d579e41ab3bd40ec9f85633506bef4bfb9
En0t <78305290+bad-cyber@users.noreply.github.com>
2026-09-23 15:39:40 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,9 +1,9 @@
 # Bot Configuration
-BOT_TOKEN = "7554992084:AAH-WMh-Ihv3Uqk39EW8PoFR3QjPkIEvS2c"
-CHANNEL_ID = "-1002296873924"
+BOT_TOKEN = "Bot_token"
+CHANNEL_ID = "-Channel_id"
 
 # Github AutoUpdate Configuration
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "git_token"
 
 # Logging Configuration
 LOG_LEVEL = "INFO"
